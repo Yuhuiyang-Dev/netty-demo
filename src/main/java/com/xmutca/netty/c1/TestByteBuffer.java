00a07d92f12aa5dc9e3038ca4a77e3b61a79546e
余辉阳 <yhy23456@163.com>
2021-12-16 23:27:03 +0800

    init

---
+++ src/main/java/com/xmutca/netty/c1/TestByteBuffer.java
@@ -0,0 +1,41 @@
+package com.xmutca.netty.c1;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * nio的例子，ByteBuffer的基本使用
+ *
+ * @author <a href="mailto:yhy23456@163.com">huiyang.yu</a>
+ * @since 2021.12.14
+ */
+@Slf4j
+public class TestByteBuffer {
+
+    public static void main(String[] args) {
+        //FileChannel
+        //1.输入输出流 2. RandomAccessFile
+        try (FileChannel channel = new FileInputStream("data.txt").getChannel()) {
+            ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+            while (true) {
+                int len = channel.read(byteBuffer);
+                log.debug("读到的字节数 {}", len);
+                if (len == -1) {
+                    break;
+                }
+                byteBuffer.flip();//切换至读模式(limit = position; position = 0; mark = -1;)
+                while (byteBuffer.hasRemaining()) {
+                    byte b = byteBuffer.get();
+                    log.debug("读到的字符 {}", (char) b);
+                }
+                byteBuffer.clear();//切换至写模式(position = 0; limit = capacity; mark = -1;)
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+}
---
+++ src/main/java/com/xmutca/netty/c1/TestByteBufferReadWrite.java
@@ -0,0 +1,36 @@
+package com.xmutca.netty.c1;
+
+import static com.xmutca.netty.c1.ByteBufferUtil.debugAll;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author <a href="mailto:yhy23456@163.com">huiyang.yu</a>
+ * @since 2021.12.14
+ */
+public class TestByteBufferReadWrite {
+
+    public static void main(String[] args) {
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+
+        buffer.put((byte) 0x61);
+        debugAll(buffer);
+
+        buffer.put(new byte[]{0x62, 0x63, 0x64});
+        debugAll(buffer);
+
+//        System.out.println(buffer.get());
+        buffer.flip();
+        System.out.println(buffer.get());
+        debugAll(buffer);
+
+        buffer.compact();//compact切换模式，此时position及其后面的数据被压缩到ByteBuffer前面去了,( (新)position = limit - position; limit = capacity)
+        debugAll(buffer);
+
+
+        buffer.put(new byte[]{0x65, 0x66});
+        debugAll(buffer);
+
+    }
+
+}

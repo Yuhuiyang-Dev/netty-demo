cb1fb3b7d72b26dd96798e8ea00dcae03634524f
余辉阳 <yhy23456@163.com>
2021-12-23 22:52:40 +0800

    netty helloWorld

---
+++ src/main/java/com/xmutca/netty/HelloClient.java
@@ -0,0 +1,38 @@
+package com.xmutca.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+import java.net.InetSocketAddress;
+
+/**
+ * @author <a href="mailto:yhy23456@163.com">huiyang.yu</a>
+ * @since 2021.12.23
+ */
+public class HelloClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        // 1. 启动类
+        new Bootstrap()
+            //2. 添加 EventLoop
+            .group(new NioEventLoopGroup())
+            //3. 选择客户端 channel 实现
+            .channel(NioSocketChannel.class)
+            //4. 添加处理器
+            .handler(new ChannelInitializer<NioSocketChannel>() {
+                @Override// 在连接建立后被调用
+                protected void initChannel(NioSocketChannel ch) throws Exception {
+                    ch.pipeline()
+                        .addLast(new StringEncoder());
+                }
+            })
+        // 5. 连接到服务器
+        .connect(new InetSocketAddress("localhost",8080))
+        .sync()
+        .channel()
+        .writeAndFlush("hello, world");
+    }
+
+}
--- src/main/java/com/xmutca/netty/HelloServer.java
+++ src/main/java/com/xmutca/netty/HelloServer.java
@@ -7,6 +7,7 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.sctp.nio.NioSctpChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 
 public class HelloServer {
@@ -14,28 +15,28 @@ public class HelloServer {
     public static void main(String[] args) {
         // 1. 启动器，负责组装 netty 组件，启动服务
         new ServerBootstrap()
-                //2. BossEventLoop ,WorkerEventLoop(selector,thread) ,group 组
-                .group(new NioEventLoopGroup())
-                //3. 选择 服务器的 ServerSocketChannel 实现
-                .channel(NioServerSocketChannel.class)// OIO BIO
-                //4. boss 负责出来连接 worker(child) 负责处理读写， 决定了 worker(child) 能执行那些操作（handler）
-                .childHandler(
-                        //5. 代表和客户端进行数据读写的通道 Initializer 初始化，负责添加别的 handler
-                        new ChannelInitializer<NioSctpChannel>() {
-                            @Override
-                            protected void initChannel(NioSctpChannel ch) throws Exception {
-                                // 6. 添加具体 handler
-                                ch.pipeline().addLast(new StringDecoder());//将 ByteBuf 转换为字符串
-                                ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {//自定义 handler
-                                    @Override// 读事件
-                                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                        // 打印上一步转换好的字符串
-                                        System.out.println(msg);
-                                    }
-                                });
-                            }
-                        })
-                //6.绑定监听端口
-                .bind(8080);
+            //2. BossEventLoop ,WorkerEventLoop(selector,thread) ,group 组
+            .group(new NioEventLoopGroup())
+            //3. 选择 服务器的 ServerSocketChannel 实现
+            .channel(NioServerSocketChannel.class)// OIO BIO
+            //4. boss 负责出来连接 worker(child) 负责处理读写， 决定了 worker(child) 能执行那些操作（handler）
+            .childHandler(
+                //5. 代表和客户端进行数据读写的通道 Initializer 初始化，负责添加别的 handler
+                new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        // 6. 添加具体 handler
+                        ch.pipeline().addLast(new StringDecoder())//将 ByteBuf 转换为字符串
+                            .addLast(new ChannelInboundHandlerAdapter() {//自定义 handler
+                                @Override// 读事件
+                                public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                    // 打印上一步转换好的字符串
+                                    System.out.println(msg);
+                                }
+                            });
+                    }
+                })
+            //7.绑定监听端口
+            .bind(8080);
     }
 }
